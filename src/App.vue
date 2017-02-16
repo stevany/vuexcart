4646d6607d7518c92a941e71437285b5173bd7bd
Maria Stevany <stevany_tan@yahoo.com>
2017-02-16 19:45:13 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+  	<CompletedTodos></CompletedTodos>
+  	<GetTodo></GetTodo>
+  	<CurrentTodos></CurrentTodos>
+  </div>
+</template>
+
+<script>
+import GetTodo from './components/GetTodo.vue'
+import CompletedTodos from './components/CompleteTodos.vue'
+import CurrentTodos from './components/CurrentTodos.vue'
+
+export default {
+  components:{
+  	GetTodo,
+  	CurrentTodos,
+  	CompletedTodos
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
